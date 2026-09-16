d820485f12b84e274d200e20248b07603ae158f7
SaifKhalifa <khsaif5@gmail.com>
2026-09-16 12:32:25 +0300

    Bank Account task
    this task is used to train on OOP, polymoriphism, inheritance, and to get used to c# syntax

---
+++ PlayGround/CurrentBankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.BankAccounts
+{
+    public class CurrentBankAccount : BankAccount
+    {
+        public CurrentBankAccount(string holderName, double startingBalance = 0) : base(holderName, startingBalance)
+        {
+            Console.WriteLine("*INFO*: New Current Bank Account is created under name: " + holderName);
+        }
+
+        public override void Withdraw(double amount)
+        {
+            double interest = amount * (1.5 / 100);
+            base.Withdraw(amount + interest);
+        }
+    }
+
+    public class SavingsBankAccount : BankAccount
+    {
+        public SavingsBankAccount(string holderName, double startingBalance = 0) : base(holderName, startingBalance)
+        {
+            Console.WriteLine("*INFO*: New Savings Bank Account is created under name: " + holderName);
+        }
+    }
+}
