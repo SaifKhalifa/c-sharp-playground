e1b944929e5ad920b1a7b2ea26c9aef3f0eb4d0b
SaifKhalifa <khsaif5@gmail.com>
2026-09-16 15:33:00 +0300

    added comments about the task

--- PlayGround/task-5-stack-queue/Program.cs
+++ PlayGround/task-5-stack-queue/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace task_5_stack_queue
+﻿/*
+    Task 5 – Stack & Queue
+    
+    Implement:
+        - A Stack-based solution for a simple problem such as checking balanced parentheses.
+        - A Queue-based solution representing a basic customer/service queue.
+*/
+
+
+namespace task_5_stack_queue
 {
     internal class Program
     {
