1e17ece3c1370466ff1e3a9560dab18b80e66d16
SaifKhalifa <khsaif5@gmail.com>
2026-09-20 23:30:30 +0300

    Add stack-based solution for balanced parentheses check
    Replaced "Hello, World!" with a stack-based implementation to
    check for balanced parentheses. Added user input handling,
    validation for null/empty strings, and logic to process the
    input string. Outputs "VALID" or "NOT VALID!" based on the
    parentheses balance.

--- PlayGround/task-5-stack-queue/Program.cs
+++ PlayGround/task-5-stack-queue/Program.cs
@@ -6,14 +6,45 @@
         - A Queue-based solution representing a basic customer/service queue.
 */
 
-
 namespace task_5_stack_queue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<char> balance = new Stack<char>();
+
+            Console.Write("your string: ");
+            string parentheses = Console.ReadLine();
+
+            if (parentheses == null || parentheses == "")
+            {
+                Console.WriteLine("\nNULL???, Please enter a valid value!");
+                // exit
+                Environment.Exit(0);
+            }
+            else
+            {
+                char[] chars = parentheses.ToArray();
+
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == '(')
+                        balance.Push(chars[i]);
+                    else if (chars[i] == ')')
+                        balance.Pop();
+                    else
+                        continue;
+                }
+
+            }
+
+            if (balance.Count == 0)
+            {
+                Console.WriteLine("VALID");
+            }
+            else
+                Console.WriteLine("NOT VALID!");
         }
     }
 }
